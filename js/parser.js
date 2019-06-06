b8daae9bec227a8a896ed52d9379874e274be6d1
Álvaro Galisteo <alvarogalisteo@outlook.es>
2019-06-06 22:31:19 +0200

    Implements #2

--- js/parser.js
+++ js/parser.js
@@ -42,39 +42,40 @@ const registers = {
 };
 
 const instructions = {
-    // R-Type
-    "SLL":   [0, 0],
-    "SRL":   [0, 2],
-    "SRA":   [0, 3],
-    "ADD":   [0, 32],
-    "ADDU":  [0, 33],
-    "AND":   [0, 34],
-    "DIV":   [0, 26],
-    "DIVU":  [0, 27],
-    "JR":    [0, 8],
-    "MFHI":  [0, 16],
-    "MTHI":  [0, 17],
-    "MFLO":  [0, 18],
-    "MTLO":  [0, 19],
-    "MULT":  [0, 24],
-    "MULTU": [0, 25],
-    "NOR":   [0, 39],
-    "XOR":   [0, 38],
-    "OR":    [0, 37],
-    "SLT":   [0, 42],
-    "SLTU":  [0, 43],
-    "SUB":   [0, 34],
-    "SUBU":  [0, 35],
-
-    //I-type
-    "ADDI":   8,
-    "ADDIU":  9,
-    "SLTI":   10,
-    "SLTIU":  11,
-    "ANDI":   12,
-    "ORI":    13,
-    "XORI":   14,
-    "LUI":    15
+    "r-type": { // R-Type
+        "SLL":   [0, 0],
+        "SRL":   [0, 2],
+        "SRA":   [0, 3],
+        "ADD":   [0, 32],
+        "ADDU":  [0, 33],
+        "AND":   [0, 34],
+        "DIV":   [0, 26],
+        "DIVU":  [0, 27],
+        "JR":    [0, 8],
+        "MFHI":  [0, 16],
+        "MTHI":  [0, 17],
+        "MFLO":  [0, 18],
+        "MTLO":  [0, 19],
+        "MULT":  [0, 24],
+        "MULTU": [0, 25],
+        "NOR":   [0, 39],
+        "XOR":   [0, 38],
+        "OR":    [0, 37],
+        "SLT":   [0, 42],
+        "SLTU":  [0, 43],
+        "SUB":   [0, 34],
+        "SUBU":  [0, 35]
+    },
+    "i-type": { //I-type
+        "ADDI":   8,
+        "ADDIU":  9,
+        "SLTI":   10,
+        "SLTIU":  11,
+        "ANDI":   12,
+        "ORI":    13,
+        "XORI":   14,
+        "LUI":    15
+    }
 };
 
 const regex = {
@@ -128,54 +129,48 @@ function parseContents(){
 // TODO
 function parseInstr(instruction){
     var binary = 0;
-    // Step 1: Split instruction in parts
-    // Remove trailing whitespace
+    // Remove trailing whitespace and convert to uppercase
     instruction = instruction.replace(/^\s+|\s+$/g,'');
     instruction = instruction.toUpperCase();
 
+    // Match instruction with instruction types and labels
     var label = /[a-zA-Z]+:/g.exec(instruction);
     instruction = matchInstruction(instruction);
-    
+
     if(instruction === null) throw "Invalid instruction";
 
     var type = instruction[0];
     var parts = instruction[1];
 
-    var opcode = instructions[parts[1]];
+    // Check if OP codes and instructions are valid
+    var opcode;
+    var ins;
+    if (type.includes("i-type")) {
+        opcode = instructions["i-type"][parts[1]];
+    } else if (type.includes("r-type")) {
+        opcode = instructions["r-type"][parts[1]];
+    }
+
     if(opcode === undefined) throw "Invalid instruction";
 
-    //console.log(type);
-
-    var ins;
-
-    // Step 2: Parse each part
+    // Parse types
     if (type.includes("i-type")){
         var immediate = 0; 
         switch (type){
-            case "i-type-dec": immediate = parts[4]; break;
-            case "i-type-hex": immediate = parseInt(parts[4], 16); break;
+            case "i-type-dec":  immediate = parts[4]; break;
+            case "i-type-hex":  immediate = parseInt(parts[4], 16); break;
             case "i-type-char": immediate = parts[4].charCodeAt(1); break;
         }
         if (immediate > 65535) throw "Invalid instruction";
+
         binary = opcode << 26 | 
             (getRegisterId(parts[2], false) << 21) | 
             (getRegisterId(parts[3], false) << 16) | 
             immediate;
-    } else if (type === "i-type-hex"){
-        if (parseInt(parts[4], 16) > 65535) { return null; }
-        binary = opcode << 26 | 
-            (getRegisterId(parts[2], false) << 21) | 
-            (getRegisterId(parts[3], false) << 16) | 
-            parseInt(parts[4], 16);
-    } else if (type === "i-type-char"){
-        if (parts[4].charCodeAt(1) > 65535) { return null; }
-        binary = opcode << 26 | 
-            (getRegisterId(parts[2], false) << 21) | 
-            (getRegisterId(parts[3], false) << 16) | 
-            parts[4].charCodeAt(1);
     } else if (type === "r-type-shift"){
         if (parts[4] > 31) { return null; }
         ins = opcode;
+
         binary = ins[0] << 26 | 
             (getRegisterId(parts[2], false) << 16) | 
             (getRegisterId(parts[3], false) << 11) | 
@@ -183,6 +178,7 @@ function parseInstr(instruction){
             ins[1];
     } else if (type === "r-type-triple"){
         ins = opcode;
+
         binary = ins[0] << 26 | 
             (getRegisterId(parts[2], false) << 21) | 
             (getRegisterId(parts[3], false) << 16) |
@@ -190,18 +186,19 @@ function parseInstr(instruction){
             ins[1];
     }  else if (type === "r-type-double"){
         ins = opcode;
+
         binary = ins[0] << 26 | 
             (getRegisterId(parts[2], false) << 21) | 
             (getRegisterId(parts[3], false) << 16) |
             ins[1];
     }  else if (type === "r-type-single"){
         ins = opcode;
+        
         binary = ins[0] << 26 | 
             (getRegisterId(parts[2], false) << 21) | 
             ins[1];
     }
 
-    // Step 4: Return number
     return [label, binary];
 }
 
@@ -234,5 +231,7 @@ function pad(n, width, z) {
     return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-console.log(pad((parseInstr("ADD $t0 $t0 5")[1] >>> 0).toString(2), 32, 0));
-console.log(getRegisterMnemonic(32));
+console.log(pad((parseInstr("ADD $t0 $t0 $a0")[1] >>> 0).toString(2), 32, 0));
+console.log(pad((parseInstr("ADDI $t0 $t0 255")[1] >>> 0).toString(2), 32, 0));
+console.log(pad((parseInstr("ADDI $t0 $t0 0xFF")[1] >>> 0).toString(2), 32, 0));
+console.log(pad((parseInstr("ADDI $t0 $t0 $a0")[1] >>> 0).toString(2), 32, 0));
